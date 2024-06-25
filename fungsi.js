c9398b4c923bdf9a35fd4dae54251745d4f9d0c3
Ahmad Habib Hamidi <ahmadhabib2b@gmail.com>
2024-06-25 17:50:51 +0700

    menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -2,3 +2,5 @@ const add = (a, b) => a + b;
 console.log(add(a, b));
 const substract = (a, b) => a - b;
 console.log(substract(a, b));
+const multiply = (a, b) => a * b;
+console.log(multiply(a, b));
