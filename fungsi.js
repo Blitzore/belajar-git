74606472bb218fdc5c202609fcfa5de66e666c7d
Ahmad Habib Hamidi <ahmadhabib2b@gmail.com>
2024-06-25 18:21:54 +0700

    menambahkan fitur eksponen

--- fungsi.js
+++ fungsi.js
@@ -1,8 +1,14 @@
 const add = (a, b) => a + b;
 console.log(add(a, b));
+
 const substract = (a, b) => a - b;
 console.log(substract(a, b));
+
 const multiplyNumber = (a, b) => a * b;
 console.log(multiplyNumber(a, b));
+
 const divide = (a, b) => a / b;
 console.log(divide(a, b));
+
+const exponent = (a, b) => a ** b;
+console.log(exponent(a, b));
